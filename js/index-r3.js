47ce255a79db22aa7b75b6d6dc4942b5927714b5
James Xu <xujames007@gmail.com>
2019-04-07 11:31:52 -0400

    reformat

--- js/index-r3.js
+++ js/index-r3.js
@@ -1,41 +1,43 @@
 $(document).ready(function () {
 
-    var slides = [1, 2, 3, 5, 6, 11, 12, 13, 14, 15, 16];
-    var target = Math.floor(Math.random() * slides.length);
+    var slides = [1, 2, 3, 5, 6, 11, 12, 13, 14, 15, 16]
+    var target = Math.floor(Math.random() * slides.length)
 
-    console.log(target, 'url("images/slide/' + slides[target] + '.jpg");');
-    $('#header').css('background-image', 'url("images/slide/' + slides[target] + '.jpg");');
+    console.log(target, 'url("images/slide/' + slides[target] + '.jpg");')
+    $('#header').css('background-image', 'url("images/slide/' + slides[target] + '.jpg");')
 
     $("#applybtn").click(function () {
-        swal({title: 'I am applying as a...',
+        swal({
+            title: 'I am applying as a...',
             showCancelButton: false,
             showCloseButton: true,
             showConfirmButton: false,
-            html:'<a href="https://register.masseyhacks.ca/" target="_blank"><button type="button" class="btn btn-large btn-wide">Hacker</button><br></a>' +
-                '<a href="https://masseyhacks.ca" target="_blank"><button type="button" disabled="true" class="btn btn-large btn-wide">Mentor/Workshop Host</button></a>'});
+            html: '<a href="https://register.masseyhacks.ca/" target="_blank"><button type="button" class="btn btn-large btn-wide">Hacker</button><br></a>' +
+                '<a href="https://masseyhacks.ca" target="_blank"><button type="button" disabled="true" class="btn btn-large btn-wide">Mentor/Workshop Host</button></a>'
+        })
     })
 
 
     function updateScroll(position) {
         switch (position) {
             case 'hidden': // Hide bar when detached from top
-                $('#nav-main').removeClass('nav-active');
-                $('#nav-main').removeClass('nav-docked');
-                $('#nav-main').addClass('nav-hidden');
+                $('#nav-main').removeClass('nav-active')
+                $('#nav-main').removeClass('nav-docked')
+                $('#nav-main').addClass('nav-hidden')
 
-                $("#nav-ham").attr("hidden", true);
+                $("#nav-ham").attr("hidden", true)
 
-                break;
+                break
             case 'body': // Body
-                $('#nav-main').css('position', 'fixed');
+                $('#nav-main').css('position', 'fixed')
 
-                $('#nav-main').removeClass('nav-docked');
-                $('#nav-main').addClass('nav-active');
+                $('#nav-main').removeClass('nav-docked')
+                $('#nav-main').addClass('nav-active')
 
-                $('#nav-main').removeClass('nav-hidden');
+                $('#nav-main').removeClass('nav-hidden')
 
                 //$('#nav-logo').attr('src', 'images/logo/logo.svg');
-                $('#bannerImg').attr('src', 'https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-blue.svg');
+                $('#bannerImg').attr('src', 'https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-blue.svg')
                 //$('#nav-logo-group').attr("hidden", false);
 
                 //$('#mobile-overlay').removeClass('overlay-dark');
@@ -47,15 +49,15 @@ $(document).ready(function () {
                     $("#nav-ham").attr("hidden", false);
                 }*/
 
-                break;
+                break
             default: // Top (Docked)
-                $('#nav-main').css('position', 'absolute');
-                $('#nav-main').addClass('nav-docked');
-                $('#nav-main').removeClass('nav-active');
-                $('#nav-main').removeClass('nav-hidden');
+                $('#nav-main').css('position', 'absolute')
+                $('#nav-main').addClass('nav-docked')
+                $('#nav-main').removeClass('nav-active')
+                $('#nav-main').removeClass('nav-hidden')
 
                 //$('#nav-logo').attr('src', 'images/logo/logo-white.svg');
-                $('#bannerImg').attr('src', 'https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg');
+                $('#bannerImg').attr('src', 'https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg')
                 //$('#nav-logo-group').attr("hidden", true);
 
                 //$('#mobile-overlay').addClass('overlay-dark');
@@ -66,97 +68,97 @@ $(document).ready(function () {
                     $("#nav-ham").attr("hidden", false);
                 }*/
 
-                break;
+                break
         }
-    };
+    }
 
     var wp = new Waypoint({
         element: document.getElementById('about'),
-        handler: function(direction) {
-            updateScroll(direction == 'down' ? 'body' : 'hidden');
+        handler: function (direction) {
+            updateScroll(direction == 'down' ? 'body' : 'hidden')
         },
         offset: '50px'
     })
 
     var wp2 = new Waypoint({
         element: document.getElementById('header'),
-        handler: function(direction) {
-            updateScroll(direction == 'down' ? 'hidden' : 'docked');
+        handler: function (direction) {
+            updateScroll(direction == 'down' ? 'hidden' : 'docked')
         },
         offset: '-50px'
     })
 
-    updateScroll('docked');
+    updateScroll('docked')
 
-    $('.js-navbar-link').on('click',function (e) {
-        e.preventDefault();
+    $('.js-navbar-link').on('click', function (e) {
+        e.preventDefault()
 
-        var target = this.hash;
-        var $target = $(target);
+        var target = this.hash
+        var $target = $(target)
 
         $('html, body').stop().animate({
             'scrollTop': $target.offset().top
         }, 500, 'swing', function () {
 
-        });
-    });
+        })
+    })
 
-    $('#year').html(new Date().getFullYear());
+    $('#year').html(new Date().getFullYear())
 
-    $(".hamburger").on("click",function(){
-        toggleOverlay();
-    });
+    $(".hamburger").on("click", function () {
+        toggleOverlay()
+    })
 
     $('a.overlayLink').click(function () {
-        toggleOverlay();
-    });
+        toggleOverlay()
+    })
 
     var bgresize = function () {
         if ($(document).width() <= 767) {
 
-            $("#accordion").removeClass("hidden");
-            $(".accordionCont").removeClass("hidden");
-            $("#full-faq").addClass("hidden");
-            $("#nav-ham").removeClass("hidden");
+            $("#accordion").removeClass("hidden")
+            $(".accordionCont").removeClass("hidden")
+            $("#full-faq").addClass("hidden")
+            $("#nav-ham").removeClass("hidden")
 
             //$("#navleft").hide();
-            $("#navright").hide();
+            $("#navright").hide()
 
-            $("#nav-inner").removeClass("container");
+            $("#nav-inner").removeClass("container")
         } else {
 
-            $("#accordion").addClass("hidden");
-            $(".accordionCont").addClass("hidden");
-            $("#full-faq").removeClass("hidden");
-            $("#nav-ham").addClass("hidden");
+            $("#accordion").addClass("hidden")
+            $(".accordionCont").addClass("hidden")
+            $("#full-faq").removeClass("hidden")
+            $("#nav-ham").addClass("hidden")
 
             //$("#navleft").show();
-            $("#navright").show();
+            $("#navright").show()
 
-            $("#nav-inner").addClass("container");
+            $("#nav-inner").addClass("container")
         }
 
-        if($("#nav-ham").hasClass("is-active")){
-            $("#nav-ham").toggleClass("is-active");
-            $(".overlay").css({top: "-100%"});
-            $("html").css({"overflow-y": "visible"});
+        if ($("#nav-ham").hasClass("is-active")) {
+            $("#nav-ham").toggleClass("is-active")
+            $(".overlay").css({top: "-100%"})
+            $("html").css({"overflow-y": "visible"})
         }
     }
 
-    bgresize();
-    $(window).resize(bgresize);
-    $(window).on("orientationchange", bgresize);
-});
+    bgresize()
+    $(window).resize(bgresize)
+    $(window).on("orientationchange", bgresize)
+})
 
 function toggleOverlay() {
-    $("#nav-ham").toggleClass("is-active");
+    $("#nav-ham").toggleClass("is-active")
 
-    if($("#nav-ham").hasClass("is-active")){
-        $(".overlay").css({top: "0"});
-        $("html").css({"overflow-y": "hidden"});
-    } else{
-        $(".overlay").css({top: "-100%"});
-        $("html").css({"overflow-y": "visible"});
+    if ($("#nav-ham").hasClass("is-active")) {
+        $(".overlay").css({top: "0"})
+        $("html").css({"overflow-y": "hidden"})
+    } else {
+        $(".overlay").css({top: "-100%"})
+        $("html").css({"overflow-y": "visible"})
     }
 }
 
@@ -165,23 +167,23 @@ $.ajax({
     url: 'schedule.json',
     dataType: 'json',
     success: function (data) {
-        var exp = '';
+        var exp = ''
         for (var i = 0; i < data.length; i++) {
-            var d = data[i];
-            var xOffset = 100 + 200 * d['xs'];
-            var width = (d['xe'] - d['xs']) * 200;
-            var yOffset = 95 + 80 * d['y'];
+            var d = data[i]
+            var xOffset = 100 + 200 * d['xs']
+            var width = (d['xe'] - d['xs']) * 200
+            var yOffset = 95 + 80 * d['y']
             exp += '<div style="left: ' + xOffset + 'px; top: ' + yOffset + 'px; width: ' + width + 'px !important; background-color: ' + d['color'] + ';" class="schedule-label">' +
                 '<div class="inner-label">' +
                 '<p class="label-caption"><b>' + d['caption'] + '</b></p>' +
                 '<p class="label-description">' + d['start'] + ' - ' + d['end'] + ' | ' + d['location'] + '</p>' +
-                '</div></div>';
+                '</div></div>'
         }
-        $('#label-container').html(exp);
+        $('#label-container').html(exp)
 
     },
     error: function (data) {
-        $('#schedule-frame').html('<b><h2>Server error occurred<br>Unable to get schedule</h2></b>');
+        $('#schedule-frame').html('<b><h2>Server error occurred<br>Unable to get schedule</h2></b>')
     }
 })
 
